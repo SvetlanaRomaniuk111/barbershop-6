9a7988bd417663daa4822dfeb7081916a3047d52
SvetlanaRomaniuk111 <126294774+SvetlanaRomaniuk111@users.noreply.github.com>
2023-04-14 23:11:53 +0300

    lesson-6

--- js/modal.js
+++ js/modal.js
@@ -1,26 +1,26 @@
 (() => {
-	const refs = {
-		openModalBtn: document.querySelectorAll("[data-modal-open]"),
-		closeModalBtn: document.querySelector("[data-modal-close]"),
-		modal: document.querySelector("[data-modal]"),
-	};
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
-	refs.openModalBtn.forEach((element) => {
-		element.addEventListener("click", toggleModal);
-	});
-	refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.forEach((element) => {
+    element.addEventListener('click', toggleModal);
+  });
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-	//* Закривання по кліку на бекдроп
+  //* Закривання по кліку на бекдроп
 
-	// refs.modal.addEventListener('click', removeModal);
-	// function removeModal(e) {
-	//   if (e.target === e.currentTarget) {
-	//     refs.modal.classList.add('is-hidden');
-	//   }
-	// }
+  refs.modal.addEventListener('click', removeModal);
+  function removeModal(e) {
+    if (e.target === e.currentTarget) {
+      refs.modal.classList.add('is-hidden');
+    }
+  }
 
-	function toggleModal() {
-		refs.modal.classList.toggle("is-hidden");
-		document.body.classList.toggle("no-scroll");
-	}
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
+  }
 })();
